e89c7c756f96c8089420f15e152e5ef0db813a08
Ritick Madaan <ritick_madaan@Riticks-MacBook-Air.local>
2026-04-24 21:28:20 +0530

    Localize portfolio image assets
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ frontend/src/assets/workSampleImages.js
@@ -0,0 +1,23 @@
+import apollo247ApolloHospitals from "@/assets/work-samples/apollo-24-7-apollo-hospitals.jpg";
+import beneKleed from "@/assets/work-samples/bene-kleed.jpg";
+import bhopuwala from "@/assets/work-samples/bhopuwala.png";
+import bucketlist from "@/assets/work-samples/bucketlist.png";
+import coxKings from "@/assets/work-samples/cox-kings.png";
+import erasmusPlus from "@/assets/work-samples/erasmus-plus.jpg";
+import incompletelyYours from "@/assets/work-samples/incompletely-yours.jpg";
+import radioCityAndDanikJagran from "@/assets/work-samples/radio-city-and-danik-jagran.png";
+import theArtOfLivingRadio from "@/assets/work-samples/the-art-of-living-radio.jpg";
+import theBeachhouseProject from "@/assets/work-samples/the-beachhouse-project.png";
+
+export const workSampleImages = {
+  coxKings,
+  theBeachhouseProject,
+  bucketlist,
+  apollo247ApolloHospitals,
+  erasmusPlus,
+  beneKleed,
+  incompletelyYours,
+  theArtOfLivingRadio,
+  bhopuwala,
+  radioCityAndDanikJagran,
+};
--- frontend/src/data/workSamples.js
+++ frontend/src/data/workSamples.js
@@ -1,11 +1,12 @@
+import { workSampleImages } from "@/assets/workSampleImages";
+
 export const workSamples = [
   {
     id: 1,
     slug: 'cox-kings',
     title: 'Cox & Kings',
     category: 'MarCom',
-    image:
-      'https://www.notion.so/image/attachment%3A54183730-b627-43e0-b1ec-f8ac9253dee8%3AScreenshot_2024-06-18_at_3.29.44_PM_(1).png?id=2895c6bc-a73d-80b5-bea2-d6e0e8996ff2&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.coxKings,
     year: 'June 2025 - Present',
     brandDescription:
       'Cox & Kings is the longest-established travel company worldwide. It served pioneers, poets, and prime ministers - from Everest expeditions to European summits. Today, Cox & Kings enters an era under Wilson & Hughes, blending 265 years of legacy while continuing to curate journeys for a new generation of explorers.',
@@ -24,8 +25,7 @@ export const workSamples = [
     slug: 'the-beachhouse-project',
     title: 'The Beachhouse Project',
     category: 'Branding',
-    image:
-      'https://www.notion.so/image/attachment%3Ac5d70238-f617-4a84-9fa2-bcd92a90ea99%3AYellow__White_Minimal_Creative_Portfolio_Presentation.png?id=1c55c6bc-a73d-80fa-90c9-eda296f3814a&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.theBeachhouseProject,
     year: '2022 - Present',
     brandDescription:
       'A creative space and community for artists, thinkers, and creators.',
@@ -43,8 +43,7 @@ export const workSamples = [
     slug: 'bucketlist',
     title: 'Bucketlist',
     category: 'Content Strategy',
-    image:
-      'https://www.notion.so/image/attachment%3Abc9ed55c-70ff-47b9-84a4-8d62a27082fd%3ABCX_Logo_-_ExCo._IG_post.png?id=1c55c6bc-a73d-8073-a90f-e6c2555abc1c&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.bucketlist,
     year: '2023 - Present',
     brandDescription:
       'A community-driven travel brand redefining experiences.',
@@ -63,8 +62,7 @@ export const workSamples = [
     slug: 'apollo-24-7-apollo-hospitals',
     title: 'Apollo 24/7 & Apollo Hospitals',
     category: 'Healthcare',
-    image:
-      'https://www.notion.so/image/attachment%3A6a696a51-34cf-4659-b730-cba976f04e1b%3A485294942_1100849915415161_596358338802351219_n.jpg?id=1c55c6bc-a73d-80cb-a960-cffed0b295b8&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.apollo247ApolloHospitals,
     year: '4-month in-office academic training (March-June)',
     brandDescription:
       "India's leading healthcare brand moving into digital health solutions.",
@@ -83,8 +81,7 @@ export const workSamples = [
     slug: 'erasmus-plus',
     title: 'Erasmus+',
     category: 'Education',
-    image:
-      'https://www.notion.so/image/attachment%3A8be99f85-eccc-432f-bea2-7f5f933e274a%3A484835765_9047482048690533_2024779294550237989_n.jpg?id=1c55c6bc-a73d-806c-b2a9-cd6699182b3c&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.erasmusPlus,
     year: 'Jan-Feb 2023',
     brandDescription:
       'A European Union program for education and exchange.',
@@ -103,8 +100,7 @@ export const workSamples = [
     slug: 'bene-kleed',
     title: 'Bene Kleed',
     category: 'Brand Storytelling Consultant',
-    image:
-      'https://www.notion.so/image/attachment%3Aaa9360e9-de66-4ca5-a930-28c8255c328e%3A484138730_651294527846175_6819081598494109862_n.jpg?id=1c55c6bc-a73d-8031-b42f-ede8b45ea795&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.beneKleed,
     year: '2023-2024',
     brandDescription:
       'A fashion and lifestyle e-commerce platform.',
@@ -122,8 +118,7 @@ export const workSamples = [
     slug: 'incompletely-yours',
     title: 'Incompletely Yours',
     category: 'Strategy & Storyboarding Consultant',
-    image:
-      'https://www.notion.so/image/attachment%3Aa3e6ce1b-3ec9-47ba-bca7-b83bd44f8029%3A482012579_10160662323182397_8678609428268426301_n.jpg?id=1c55c6bc-a73d-8042-a2c8-fe044c198760&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.incompletelyYours,
     year: '2024',
     brandDescription: "India's first dedicated podcast on divorce.",
     challenge:
@@ -141,8 +136,7 @@ export const workSamples = [
     slug: 'the-art-of-living-radio',
     title: 'The Art of Living Radio',
     category: 'Digital Media',
-    image:
-      'https://www.notion.so/image/attachment%3A4e9d4397-a419-4f21-83c7-f6f23ee8df2a%3A472791839_1011674770974934_4523481469975011235_n.jpg?id=1c55c6bc-a73d-80b7-95a5-f6d8bd81b3c8&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.theArtOfLivingRadio,
     year: '2020-2021',
     brandDescription:
       'A global spiritual movement meeting digital audio, as a Bhopuwala client.',
@@ -161,8 +155,7 @@ export const workSamples = [
     slug: 'bhopuwala',
     title: 'Bhopuwala',
     category: 'Podcast Production',
-    image:
-      'https://www.notion.so/image/attachment%3A654e681d-f8dd-423d-8bd1-c0a67d5ae89a%3Abhopuwala-creative-solutions.png.png?id=1c55c6bc-a73d-8025-b0d1-cf5df8b0b628&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.bhopuwala,
     year: '2020-2021',
     brandDescription:
       'Bhopuwala is a story-first audio powerhouse blending radio, podcasts, jingles, and voiceovers.',
@@ -181,8 +174,7 @@ export const workSamples = [
     slug: 'radio-city-and-danik-jagran',
     title: 'Radio City and Danik Jagran',
     category: 'Radio & Journalism',
-    image:
-      'https://www.notion.so/image/attachment%3A316ad8ba-33ef-495b-b31c-8eb460c140f4%3AScreenshot_2025-03-29_at_2.42.17_PM.png?id=1c55c6bc-a73d-809d-b102-eff345fcff9e&table=block&spaceId=870462a0-399f-4c39-a7ed-9e8cce06f6c3&width=520&userId=&cache=v2',
+    image: workSampleImages.radioCityAndDanikJagran,
     year: '2019-2020 (college academic internship, 2 months each during breaks)',
     brandDescription:
       "One of India's leading FM stations and India's largest-selling Hindi daily newspaper.",
